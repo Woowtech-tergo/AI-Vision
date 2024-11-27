b04d755edbe625c94cbd70f90d10fff452fc8624
gustavofelicidade <gustavosnik95@gmail.com>
2024-11-27 15:45:47 -0300

    Adding to RAILWAY server 0.0.7.4.2  main.py HELLO WORLD trying to deploy on vercel 0.0.2

--- main.py
+++ main.py
@@ -1,12 +1,17 @@
+import os
 import gradio as gr
 
 def greet(name, intensity):
     return "Hello, " + name + "!" * int(intensity)
 
+# Interface do Gradio
 demo = gr.Interface(
     fn=greet,
     inputs=["text", "slider"],
     outputs=["text"],
 )
 
-demo.launch()
+if __name__ == "__main__":
+    # Use a porta fornecida pela variável de ambiente PORT
+    port = int(os.getenv("PORT", 7860))
+    demo.launch(server_name="0.0.0.0", server_port=port)
