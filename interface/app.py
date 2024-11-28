b4410272ea92aa4ab80f4b58e5e55f6d8565f358
gustavofelicidade <gustavosnik95@gmail.com>
2024-11-28 16:50:26 -0300

    ADD DeepSort Tracking IA Modelo 0.0.9

--- interface/app.py
+++ interface/app.py
@@ -1,191 +1,233 @@
-import gradio as gr
+from ultralytics import YOLO
 import cv2
 import numpy as np
-import torch
-import os
+import tempfile
+from pathlib import Path
+from tqdm.auto import tqdm
 
-# Importações necessárias para o modelo de contagem de pessoas
-from Modelos.ContadorDePessoasEmVideo.Person import Person
-import time
-from random import randint
+import deep_sort.deep_sort.deep_sort as ds
 
-def counting_people(video_input):
-    # Verifica se o vídeo foi enviado através da interface ou se é um fluxo da webcam
-    if video_input is None:
-        return "Nenhum vídeo fornecido."
-    else:
-        # Verifica se o vídeo é um caminho válido
-        if isinstance(video_input, str):
-            video_path = video_input
-        else:
-            # Se for um objeto do Gradio, obtemos o caminho temporário do arquivo
-            video_path = video_input.name
+import gradio as gr
 
-        # Verifica se o arquivo existe
-        if not os.path.exists(video_path):
-            return "Arquivo de vídeo não encontrado."
+# Controla se o processamento deve ser interrompido
+should_continue = True
 
-        cap = cv2.VideoCapture(video_path)
 
-    # Iniciais
-    leftCounter = 0
-    rightCounter = 0
+def get_detectable_classes(model_file):
+    """Obtém as classes detectáveis de um arquivo de modelo fornecido.
 
-    # Verifica se a captura foi aberta corretamente
-    if not cap.isOpened():
-        return "Não foi possível abrir o vídeo."
+    Parâmetros:
+    - model_file: Nome do arquivo do modelo.
 
-    # Configurações de vídeo
-    w = int(cap.get(cv2.CAP_PROP_FRAME_WIDTH))
-    h = int(cap.get(cv2.CAP_PROP_FRAME_HEIGHT))
-    if w == 0 or h == 0:
-        return "Não foi possível obter as dimensões do vídeo."
+    Retorna:
+    - class_names: Lista dos nomes das classes detectáveis.
+    """
+    model = YOLO(model_file)
+    class_names = list(model.names.values())  # Obtém diretamente a lista de nomes das classes
+    del model  # Exclui a instância do modelo para liberar recursos
+    return class_names
 
-    frameArea = h * w
-    areaTH = frameArea * 0.003
 
-    # Linhas de detecção
-    leftmostLine = int(1.0 / 6 * w)
-    rightmostLine = int(5.0 / 6 * w)
-    leftmostLimit = int(1.0 / 12 * w)
-    rightmostLimit = int(11.0 / 12 * w)
+# Função para interromper o processamento de vídeo
+def stop_processing():
+    global should_continue
+    should_continue = False  # Altera a variável global para parar o processamento
+    return "Tentando interromper o processamento..."
 
-    # Configurações de cores
-    leftmostLineColor = (255, 0, 0)
-    rightmostLineColor = (0, 0, 255)
-    font = cv2.FONT_HERSHEY_SIMPLEX
 
-    # Subtrator de fundo
-    backgroundSubtractor = cv2.createBackgroundSubtractorMOG2()
+# Função para iniciar o processamento de vídeo
+# gr.Progress(track_tqdm=True) captura a barra de progresso tqdm para exibição na interface GUI
+def start_processing(input_path, output_path, detect_class, model, progress=gr.Progress(track_tqdm=True)):
+    global should_continue
+    should_continue = True
 
-    # Kernels para morfologia
-    kernelOp = np.ones((3, 3), np.uint8)
-    kernelCl = np.ones((9, 9), np.uint8)
+    detect_class = int(detect_class)
+    model = YOLO(model)
+    tracker = ds.DeepSort("deep_sort/deep_sort/deep/checkpoint/ckpt.t7")
+    output_video_path = detect_and_track(input_path, output_path, detect_class, model, tracker)
+    return output_video_path, output_video_path
 
-    # Variáveis
-    persons = []
-    max_p_age = 5
-    pid = 1
 
-    # Criação do objeto para salvar o vídeo processado
-    output_frames = []
+def putTextWithBackground(
+        img,
+        text,
+        origin,
+        font=cv2.FONT_HERSHEY_SIMPLEX,
+        font_scale=1,
+        text_color=(255, 255, 255),
+        bg_color=(0, 0, 0),
+        thickness=1,
+):
+    """Desenha texto com fundo.
 
-    while cap.isOpened():
-        ret, frame = cap.read()
+    :param img: Imagem de entrada.
+    :param text: Texto a ser desenhado.
+    :param origin: Coordenadas do canto superior esquerdo do texto.
+    :param font: Tipo de fonte.
+    :param font_scale: Tamanho da fonte.
+    :param text_color: Cor do texto.
+    :param bg_color: Cor do fundo.
+    :param thickness: Espessura da linha do texto.
+    """
+    # Calcula o tamanho do texto
+    (text_width, text_height), _ = cv2.getTextSize(text, font, font_scale, thickness)
 
-        if not ret:
+    # Desenha o retângulo de fundo
+    bottom_left = origin
+    top_right = (origin[0] + text_width, origin[1] - text_height - 5)  # Subtrai 5 para adicionar margem
+    cv2.rectangle(img, bottom_left, top_right, bg_color, -1)
+
+    # Desenha o texto sobre o retângulo
+    text_origin = (origin[0], origin[1] - 5)  # Ajusta a posição para adicionar margem
+    cv2.putText(
+        img,
+        text,
+        text_origin,
+        font,
+        font_scale,
+        text_color,
+        thickness,
+        lineType=cv2.LINE_AA,
+    )
+
+
+def extract_detections(results, detect_class):
+    """
+    Extrai e processa informações de detecção dos resultados do modelo.
+    - results: Resultados da previsão do modelo YoloV8, incluindo posição, categoria e confiança dos objetos detectados.
+    - detect_class: Índice da classe alvo a ser extraída.
+    Referência: https://docs.ultralytics.com/modes/predict/#working-with-results
+    """
+    # Inicializa um array numpy bidimensional vazio para armazenar as posições dos objetos detectados
+    detections = np.empty((0, 4))
+
+    confarray = []  # Inicializa uma lista vazia para armazenar os valores de confiança dos objetos detectados.
+
+    # Itera sobre os resultados das detecções
+    for r in results:
+        for box in r.boxes:
+            # Verifica se a classe detectada corresponde à classe desejada e extrai informações
+            if box.cls[0].int() == detect_class:
+                x1, y1, x2, y2 = box.xywh[0].int().tolist()  # Extrai as posições e converte para lista de inteiros.
+                conf = round(box.conf[0].item(), 2)  # Extrai a confiança e arredonda para duas casas decimais.
+                detections = np.vstack(
+                    (detections, np.array([x1, y1, x2, y2])))  # Adiciona as posições ao array de detecções.
+                confarray.append(conf)  # Adiciona a confiança à lista.
+    return detections, confarray  # Retorna as posições e as confianças.
+
+
+# Processamento de vídeo
+def detect_and_track(input_path: str, output_path: str, detect_class: int, model, tracker) -> Path:
+    """
+    Processa o vídeo, detecta e rastreia os objetos.
+    - input_path: Caminho para o arquivo de vídeo de entrada.
+    - output_path: Caminho para salvar o vídeo processado.
+    - detect_class: Índice da classe alvo a ser detectada e rastreada.
+    - model: Modelo utilizado para detecção de objetos.
+    - tracker: Modelo utilizado para rastreamento de objetos.
+    """
+    global should_continue
+    cap = cv2.VideoCapture(input_path)  # Abre o arquivo de vídeo com OpenCV.
+    if not cap.isOpened():  # Verifica se o vídeo foi aberto com sucesso.
+        print(f"Erro ao abrir o arquivo de vídeo {input_path}")
+        return None
+
+    total_frames = int(cap.get(cv2.CAP_PROP_FRAME_COUNT))  # Obtém o número total de quadros do vídeo
+    fps = cap.get(cv2.CAP_PROP_FPS)  # Obtém a taxa de quadros do vídeo
+    size = (int(cap.get(cv2.CAP_PROP_FRAME_WIDTH)),
+            int(cap.get(cv2.CAP_PROP_FRAME_HEIGHT)))  # Obtém a resolução do vídeo (largura e altura).
+    output_video_path = Path(output_path) / "output.avi"  # Define o caminho de saída para o vídeo processado.
+
+    # Configura o formato de codificação do vídeo como AVI no formato XVID
+    fourcc = cv2.VideoWriter_fourcc(*"XVID")
+    output_video = cv2.VideoWriter(output_video_path.as_posix(), fourcc, fps, size,
+                                   isColor=True)  # Cria um objeto VideoWriter para salvar o vídeo.
+
+    # Lê e processa cada quadro do vídeo
+    # Mostra o progresso utilizando tqdm
+    for _ in tqdm(range(total_frames)):
+        # Interrompe o processamento caso o botão "Stop" seja pressionado na GUI
+        if not should_continue:
+            print('Interrompendo o processamento')
             break
 
-        for per in persons:
-            per.age_one()
+        success, frame = cap.read()  # Lê quadro por quadro do vídeo
 
-        # Subtração de fundo
-        fgmask = backgroundSubtractor.apply(frame)
+        # Se a leitura falhar (fim do vídeo ou erro), interrompe o loop
+        if not success:
+            break
 
-        # Aplicação de threshold para remover sombras
-        ret2, imBin = cv2.threshold(fgmask, 127, 255, cv2.THRESH_BINARY)
-        # Operações morfológicas
-        mask = cv2.morphologyEx(imBin, cv2.MORPH_OPEN, kernelOp)
-        mask = cv2.morphologyEx(mask, cv2.MORPH_CLOSE, kernelCl)
+        # Realiza a detecção de objetos no quadro atual utilizando o modelo YoloV8.
+        results = model(frame, stream=True)
 
-        # Encontra contornos
-        contours, hierarchy = cv2.findContours(mask, cv2.RETR_EXTERNAL, cv2.CHAIN_APPROX_SIMPLE)
+        # Extrai informações de detecção dos resultados.
+        detections, confarray = extract_detections(results, detect_class)
 
-        for cnt in contours:
-            area = cv2.contourArea(cnt)
-            if area > areaTH:
-                # Centro de massa
-                M = cv2.moments(cnt)
-                if M['m00'] == 0:
-                    continue
-                cx = int(M['m10'] / M['m00'])
-                cy = int(M['m01'] / M['m00'])
+        # Realiza o rastreamento dos objetos detectados com o modelo DeepSort.
+        resultsTracker = tracker.update(detections, confarray, frame)
 
-                x, y, w_box, h_box = cv2.boundingRect(cnt)
+        for x1, y1, x2, y2, Id in resultsTracker:
+            x1, y1, x2, y2 = map(int, [x1, y1, x2, y2])  # Converte as posições para inteiros.
 
-                newPerson = True
-                if leftmostLimit <= cx <= rightmostLimit:
-                    for person in persons:
-                        if abs(x - person.getX()) <= w_box and abs(y - person.getY()) <= h_box:
-                            newPerson = False
-                            person.updateCoords(cx, cy)
+            # Desenha bounding boxes e texto
+            cv2.rectangle(frame, (x1, y1), (x2, y2), (255, 0, 255), 3)
+            putTextWithBackground(frame, str(int(Id)), (max(-10, x1), max(40, y1)), font_scale=1.5,
+                                  text_color=(255, 255, 255), bg_color=(255, 0, 255))
 
-                            if person.goingLeft(rightmostLine, leftmostLine):
-                                leftCounter += 1
-                            elif person.goingRight(rightmostLine, leftmostLine):
-                                rightCounter += 1
-                            break
+        output_video.write(frame)  # Escreve o quadro processado no arquivo de saída.
 
-                        if person.getState() == '1':
-                            if person.getDir() == 'right' and person.getX() > rightmostLimit:
-                                person.setDone()
-                            elif person.getDir() == 'left' and person.getX() < leftmostLimit:
-                                person.setDone()
+    output_video.release()  # Libera o objeto VideoWriter.
+    cap.release()  # Libera o arquivo de vídeo.
 
-                        if person.timedOut():
-                            index = persons.index(person)
-                            persons.pop(index)
-                            del person
+    print(f'O diretório de saída é: {output_video_path}')
+    return output_video_path
 
-                    if newPerson:
-                        person = Person(pid, cx, cy, max_p_age)
-                        persons.append(person)
-                        pid += 1
-
-                cv2.circle(frame, (cx, cy), 5, (0, 0, 255), -1)
-                img = cv2.rectangle(frame, (x, y), (x + w_box, y + h_box), (255, 0, 0), 2)
-
-        # Informações no frame
-        leftMsg = 'Left: ' + str(leftCounter)
-        rightMsg = 'Right: ' + str(rightCounter)
-        cv2.polylines(frame, [np.array([[rightmostLine, 0], [rightmostLine, h]])], False, rightmostLineColor, thickness=2)
-        cv2.polylines(frame, [np.array([[leftmostLine, 0], [leftmostLine, h]])], False, leftmostLineColor, thickness=2)
-        cv2.putText(frame, leftMsg, (10, 40), font, 0.5, leftmostLineColor, 1, cv2.LINE_AA)
-        cv2.putText(frame, rightMsg, (10, 90), font, 0.5, rightmostLineColor, 1, cv2.LINE_AA)
-
-        # Armazena o frame processado
-        output_frames.append(frame)
-
-    cap.release()
-    # cv2.destroyAllWindows()
-
-    if not output_frames:
-        return "Nenhum frame foi processado."
-
-    # Salva o vídeo processado em um arquivo temporário
-    output_path = 'output.mp4'
-    fourcc = cv2.VideoWriter_fourcc(*'mp4v')
-    fps = 20  # Pode ajustar conforme necessário
-    height, width, layers = output_frames[0].shape
-    out = cv2.VideoWriter(output_path, fourcc, fps, (width, height))
-
-    for frame in output_frames:
-        out.write(frame)
-
-    out.release()
-
-    return output_path
-
-# Configuração da interface Gradio
-def create_app():
-    with gr.Blocks() as app:
-        gr.Markdown("# Contador de Pessoas em Vídeo")
-        gr.Markdown("### Carregue um vídeo para iniciar a contagem de pessoas.")
-
-        with gr.Row():
-            video_input = gr.Video(label="Selecione um Vídeo")
-            output_video = gr.Video(label="Vídeo Processado")
-        start_button = gr.Button("Iniciar Contagem de Pessoas")
-
-        start_button.click(
-            fn=counting_people,
-            inputs=video_input,
-            outputs=output_video
-        )
-
-    return app
-
-app = create_app()
 
 if __name__ == "__main__":
-    app.launch()
+    # Lista oficial de modelos YoloV8 e V9. O modelo será baixado automaticamente na primeira execução.
+    model_list = ["yolov9c.pt", "yolov9e", "yolov8n.pt", "yolov8s.pt", "yolov8m.pt", "yolov8l.pt", "yolov8x.pt"]
+
+    # Obtém todas as classes detectáveis pelo modelo YoloV8. Por padrão, usa o primeiro modelo da lista.
+    detect_classes = get_detectable_classes(model_list[0])
+
+    # Exemplo de entrada para a interface Gradio, incluindo um caminho de vídeo de teste, um diretório de saída temporário, uma classe de detecção e um modelo.
+    examples = [["test.mp4", tempfile.mkdtemp(), detect_classes[0], model_list[0]]]
+
+    # Cria uma interface GUI utilizando Gradio
+    with gr.Blocks() as demo:
+        with gr.Tab("Tracking"):
+            gr.Markdown(
+                """
+                # Detecção e Rastreamento de Objetos
+                Baseado em OpenCV + YoloV8 + DeepSort
+                """
+            )
+            with gr.Row():
+                with gr.Column():
+                    input_path = gr.Video(label="Vídeo de Entrada")  # Controle para upload de vídeo
+                    model = gr.Dropdown(model_list, value=0, label="Modelo")  # Menu suspenso para seleção de modelo
+                    detect_class = gr.Dropdown(detect_classes, value=0, label="Classe",
+                                               type='index')  # Menu suspenso para seleção de classe alvo
+                    output_dir = gr.Textbox(label="Diretório de Saída",
+                                            value=tempfile.mkdtemp())  # Caixa de texto para especificar o diretório de saída
+                    with gr.Row():
+                        start_button = gr.Button("Iniciar Processamento")  # Botão para iniciar o processamento
+                        stop_button = gr.Button("Interromper Processamento")  # Botão para interromper o processamento
+                with gr.Column():
+                    output = gr.Video()  # Controle para exibir o vídeo processado
+                    output_path = gr.Textbox(
+                        label="Caminho do Arquivo de Saída")  # Caixa de texto para exibir o caminho do arquivo de saída
+                    gr.Examples(
+                        examples,
+                        label="Exemplos",
+                        inputs=[input_path, output_dir, detect_class, model],
+                        outputs=[output, output_path],
+                        fn=start_processing,
+                        cache_examples=False,
+                    )
+        # Conecta os botões às funções de processamento
+        start_button.click(start_processing, inputs=[input_path, output_dir, detect_class, model],
+                           outputs=[output, output_path])
+        stop_button.click(stop_processing)
+
+    demo.launch()
--- main.py
+++ main.py
@@ -1,7 +1,148 @@
-import os
-from interface.app import app
+import tempfile
+from pathlib import Path
+import numpy as np
+import cv2 # opencv-python
+from ultralytics import YOLO
+
+import deep_sort.deep_sort.deep_sort as ds
+
+
+def putTextWithBackground(img, text, origin, font=cv2.FONT_HERSHEY_SIMPLEX, font_scale=1, text_color=(255, 255, 255),
+                          bg_color=(0, 0, 0), thickness=1):
+    """Desenha texto com fundo.
+
+    :param img: Imagem de entrada.
+    :param text: Texto a ser desenhado.
+    :param origin: Coordenadas do canto superior esquerdo do texto.
+    :param font: Tipo de fonte.
+    :param font_scale: Tamanho da fonte.
+    :param text_color: Cor do texto.
+    :param bg_color: Cor do fundo.
+    :param thickness: Espessura da linha do texto.
+    """
+    # Calcula o tamanho do texto
+    (text_width, text_height), _ = cv2.getTextSize(text, font, font_scale, thickness)
+
+    # Desenha um retângulo de fundo
+    bottom_left = origin
+    top_right = (origin[0] + text_width, origin[1] - text_height - 5)  # Subtrai 5 para adicionar alguma margem
+    cv2.rectangle(img, bottom_left, top_right, bg_color, -1)
+
+    # Desenha o texto sobre o retângulo
+    text_origin = (origin[0], origin[1] - 5)  # Ajusta a posição para adicionar margem
+    cv2.putText(img, text, text_origin, font, font_scale, text_color, thickness, lineType=cv2.LINE_AA)
+
+
+def extract_detections(results, detect_class):
+    """
+    Extrai e processa informações de detecção dos resultados do modelo.
+    - results: Resultados da previsão do modelo YoloV8, incluindo posição, categoria e confiança dos objetos detectados.
+    - detect_class: Índice da categoria alvo a ser extraída.
+    Referência: https://docs.ultralytics.com/modes/predict/#working-with-results
+    """
+
+    # Inicializa um array numpy bidimensional vazio para armazenar as posições dos objetos detectados
+    # Caso não haja objetos detectados da categoria desejada, a inicialização evita erros no tracker
+    detections = np.empty((0, 4))
+
+    confarray = []  # Inicializa uma lista vazia para armazenar os valores de confiança dos objetos detectados.
+
+    # Itera sobre os resultados das detecções
+    # Referência: https://docs.ultralytics.com/modes/predict/#working-with-results
+    for r in results:
+        for box in r.boxes:
+            # Verifica se a categoria detectada corresponde à categoria desejada e extrai informações
+            if box.cls[0].int() == detect_class:
+                x1, y1, x2, y2 = box.xywh[0].int().tolist()  # Extrai as posições e converte para lista de inteiros.
+                conf = round(box.conf[0].item(),
+                             2)  # Extrai a confiança, converte para float e arredonda para duas casas decimais.
+                detections = np.vstack(
+                    (detections, np.array([x1, y1, x2, y2])))  # Adiciona as posições ao array de detecções.
+                confarray.append(conf)  # Adiciona a confiança à lista.
+    return detections, confarray  # Retorna as posições e as confianças.
+
+
+# Processamento de vídeo
+def detect_and_track(input_path: str, output_path: str, detect_class: int, model, tracker) -> Path:
+    """
+    Processa o vídeo, detecta e rastreia os objetos.
+    - input_path: Caminho para o arquivo de vídeo de entrada.
+    - output_path: Caminho para salvar o vídeo processado.
+    - detect_class: Índice da categoria alvo a ser detectada e rastreada.
+    - model: Modelo utilizado para detecção de objetos.
+    - tracker: Modelo utilizado para rastreamento de objetos.
+    """
+    cap = cv2.VideoCapture(input_path)  # Abre o arquivo de vídeo com OpenCV.
+    if not cap.isOpened():  # Verifica se o vídeo foi aberto com sucesso.
+        print(f"Erro ao abrir o arquivo de vídeo {input_path}")
+        return None
+
+    fps = cap.get(cv2.CAP_PROP_FPS)  # Obtém a taxa de quadros do vídeo.
+    size = (int(cap.get(cv2.CAP_PROP_FRAME_WIDTH)),
+            int(cap.get(cv2.CAP_PROP_FRAME_HEIGHT)))  # Obtém a resolução do vídeo (largura e altura).
+    output_video_path = Path(output_path) / "output.avi"  # Define o caminho de saída para o vídeo processado.
+
+    # Configura o formato de codificação do vídeo como um arquivo AVI no formato XVID
+    # Caso deseje usar o codec h264 ou salvar em outro formato, pode ser necessário baixar o openh264-1.8.0
+    # Link para download: https://github.com/cisco/openh264/releases/tag/v1.8.0
+    # Após o download, coloque o arquivo DLL no diretório atual
+    fourcc = cv2.VideoWriter_fourcc(*"XVID")
+    output_video = cv2.VideoWriter(output_video_path.as_posix(), fourcc, fps, size,
+                                   isColor=True)  # Cria um objeto VideoWriter para salvar o vídeo.
+
+    # Lê e processa cada frame do vídeo
+    while True:
+        success, frame = cap.read()  # Lê frame por frame do vídeo.
+
+        # Interrompe o loop caso não consiga ler o frame (fim do vídeo ou erro).
+        if not success:
+            break
+
+        # Realiza detecção de objetos no frame atual utilizando o modelo YoloV8.
+        results = model(frame, stream=True)
+
+        # Extrai informações de detecção dos resultados.
+        detections, confarray = extract_detections(results, detect_class)
+
+        # Realiza rastreamento dos objetos detectados com o modelo DeepSort.
+        resultsTracker = tracker.update(detections, confarray, frame)
+
+        for x1, y1, x2, y2, Id in resultsTracker:
+            x1, y1, x2, y2 = map(int, [x1, y1, x2, y2])  # Converte as posições para inteiros.
+
+            # Desenha bounding boxes e texto
+            cv2.rectangle(frame, (x1, y1), (x2, y2), (255, 0, 255), 3)
+            putTextWithBackground(frame, str(int(Id)), (max(-10, x1), max(40, y1)), font_scale=1.5,
+                                  text_color=(255, 255, 255), bg_color=(255, 0, 255))
+
+        output_video.write(frame)  # Escreve o frame processado no arquivo de saída.
+
+    output_video.release()  # Libera o objeto VideoWriter.
+    cap.release()  # Libera o arquivo de vídeo.
+
+    print(f'O diretório de saída é: {output_video_path}')
+    return output_video_path
+
 
 if __name__ == "__main__":
-    # Use a porta fornecida pela variável de ambiente PORT
-    port = int(os.getenv("PORT", 7860))
-    app.launch(server_name="0.0.0.0", server_port=port)
+    # Define o caminho do vídeo de entrada.
+    ######
+    input_path = "test.mp4"
+    ######
+
+    # Diretório de saída, por padrão é o diretório temporário do sistema.
+    output_path = tempfile.mkdtemp()  # Cria um diretório temporário para salvar o vídeo processado.
+
+    # Carrega os pesos do modelo YOLOv8
+    model = YOLO("yolov8n.pt")
+
+    # Define a categoria de objeto a ser detectada e rastreada
+    # O primeiro índice do modelo oficial YOLOv8 é 'person'
+    detect_class = 0
+    print(f"Detectando {model.names[detect_class]}")  # model.names retorna todas as categorias suportadas pelo modelo.
+
+    # Carrega o modelo DeepSort
+    tracker = ds.DeepSort("deep_sort/deep_sort/deep/checkpoint/ckpt.t7")
+
+    detect_and_track(input_path, output_path, detect_class, model, tracker)
+
